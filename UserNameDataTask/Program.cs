e26c592caad147aeb4e7c706612d2dbf2067c53e
Kristjan Mändmets <kristjan.mandmets@ppe.ee>
2022-05-30 15:39:50 +0300

    add new projects

---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            //programm küsib kasutaja eesnime;
+            //programm küsib kasutajalt numbrit 1-3;
+            //kui kasutaja sisestab '1', siis kuvatakse kasutaja eesnimi targurpidi
+            //kui kasutaja valib 2, siis kuvatakse kasutaja eesnime esimene täht
+            //kui kasutaja valib 3, siis kuvatakse kasutaja kasutaja eesnime pikkus
+
+            Console.WriteLine("Sisesta oma eesnimi:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Sisesta number 1 kuni 3.");
+            char userNumber = Convert.ToChar(Console.ReadLine());
+
+            //kontroll millist numbrit valis kas 1, 2, 3 
+
+
+            switch (userNumber)
+            {
+                case '1':
+                    reversedName(userName);
+                    break;
+
+                case '2':
+                    firstLetterName(userName);
+                    break;
+
+                case '3':
+                    lengthName(userName);
+                    break;
+
+                default:
+                    Console.WriteLine("Kena päeva!");
+                    break;
+
+            }
+        }
+
+        public static void reversedName(string userInput)
+
+        {
+            for (int i = userInput.Length - 1; i >= 0; i--)
+
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+        public static void firstLetterName(string userInput)
+
+        {
+            Console.WriteLine($"Sinu eesnime esimene täht on {userInput[0]}");
+        }
+
+        public static void lengthName(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sümbolit pikk.");
+        }
+	}
+}
